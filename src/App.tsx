7f397e749d543334a4473702d1ec091ef8b4166e
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2020-08-13 20:58:40 +0300

    version02

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
@@ -6,8 +6,77 @@ import { HomeWork2 } from "./componets/HomeWork-2/HomeWork-2";
 import HomeWork3 from "./componets/HomeWork-3/HomeWork3";
 import { Route, NavLink } from "react-router-dom";
 import { Header } from "./componets/Header/Header";
+import Box from "@material-ui/core/Box/Box";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Divider,
+  ListItem,
+  ListItemIcon,
+  List,
+  ListItemText,
+} from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
+//CSS STYLES
+
+const useStyles = makeStyles({
+  drawer: {
+    width: 250,
+    height: "30rem",
+    background: "#511",
+  },
+  menuItem: {
+    color: "tan",
+  },
+});
+
+const menuItem = [
+  {
+    listIcon: <HomeIcon />,
+    listText: "Home",
+  },
+  {
+    listIcon: <ReorderIcon />,
+    listText: "PreJunior",
+  },
+  {
+    listIcon: <ReorderIcon />,
+    listText: "Junior",
+  },
+  {
+    listIcon: <ReorderIcon />,
+    listText: "Junior+",
+  },
+];
 function App() {
+  const classes = useStyles();
+  const [state, setState] = useState({
+    right: false,
+  });
+
+  const toggleSlider = (slider: any, open: any) => () => {
+    setState({ ...state, [slider]: open });
+  };
+
+  const sideList = (slider: any) => (
+    <Box className={classes.drawer}>
+      <List>
+        {menuItem.map((lsItem, key) => (
+          <ListItem button key={key}>
+            <ListItemIcon className={classes.menuItem}>
+              {lsItem.listIcon}
+            </ListItemIcon>
+            <ListItemText
+              className={classes.menuItem}
+              primary={lsItem.listText}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <div className="App">
       <Header />
--- src/componets/Header/Header.tsx
+++ src/componets/Header/Header.tsx
@@ -6,11 +6,21 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles } from "@material-ui/core/styles";
+
+// CSS STYLES
+
+const useStyles = makeStyles({
+  appBarc: {
+    background: "#511",
+  },
+});
 
 export const Header = () => {
+  const classes = useStyles();
   return (
     <div className={style.header}>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.appBarc}>
         <Toolbar>
           <IconButton edge="start" color="inherit" aria-label="menu">
             <MenuIcon />
