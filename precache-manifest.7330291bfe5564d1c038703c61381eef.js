a020815126d1c297369003ebcc0d5e895f972432
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2020-08-13 18:31:58 +0300

    Updates

---
+++ precache-manifest.7330291bfe5564d1c038703c61381eef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f20f488fe740e83db83db4bafdfb60",
+    "url": "/HomeWork-Ignat/index.html"
+  },
+  {
+    "revision": "3d1ca779764269658061",
+    "url": "/HomeWork-Ignat/static/css/main.e773ffbd.chunk.css"
+  },
+  {
+    "revision": "483881b5b92c7ce675e5",
+    "url": "/HomeWork-Ignat/static/js/2.2b946450.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HomeWork-Ignat/static/js/2.2b946450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1ca779764269658061",
+    "url": "/HomeWork-Ignat/static/js/main.addfe642.chunk.js"
+  },
+  {
+    "revision": "fe5b043d7ab52df1f834",
+    "url": "/HomeWork-Ignat/static/js/runtime-main.2778a53a.js"
+  }
+]);
